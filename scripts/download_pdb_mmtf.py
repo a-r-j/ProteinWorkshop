56652115c32136d5bcfece45c7a76f05202ca756
Alex Morehead <alex.morehead@gmail.com>
2023-08-26 13:32:41 -0500

    Make download_pdb_mmtf script more verbose and user-friendly

--- scripts/download_pdb_mmtf.py
+++ scripts/download_pdb_mmtf.py
@@ -34,10 +34,17 @@ def download_pdb_mmtf(create_tar: bool = True):
 
     # Download all PDB IDs with parallelized HTTP requests
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
+        num_requests = len(pdb_ids)
         pbar = tqdm(pdb_ids)
         for pdb_id in pbar:
-            pbar.set_description(f"Downloading PDB {pdb_id}")
-            executor.submit(rcsb.fetch, pdb_id, "mmtf", mmtf_dir)
+            pbar.set_description(f"Submitting PDB download request for {pdb_id}")
+            futures.append(executor.submit(rcsb.fetch, pdb_id, "mmtf", mmtf_dir))
+        pbar = tqdm(concurrent.futures.as_completed(futures))
+        for request_index, future in enumerate(pbar):
+            pbar.set_description(f"Waiting for PDB download request #{request_index + 1}/{num_requests} to complete")
+            # Wait for the future to complete
+            result = future.result()
 
     if create_tar:
         # Create .tar archive file from MMTF files in directory
@@ -49,15 +56,13 @@ def download_pdb_mmtf(create_tar: bool = True):
 
     ### File access for analysis ###
 
-    # Iterate over all files in archive
+    # Iterate over all files in archive;
     # Instead of extracting the files from the archive,
-    # the .tar file is directly accessed
-    #with tarfile.open(mmtf_dir+".tar", mode="r") as file:
-    #    for member in file.getnames():
-    #        mmtf_file = mmtf.MMTFFile.read(file.extractfile(member))
-            ###
-            # Do some fancy stuff with the data...
-            ###
+    # the `.tar` file is directly accessed
+    # with tarfile.open(f"{mmtf_dir}.tar", mode="r") as file:
+        # for member in file.getnames():
+            # mmtf_file = mmtf.MMTFFile.read(file.extractfile(member))
+            ## Do some fancy stuff with the data...
 
 if __name__ == "__main__":
     download_pdb_mmtf()
