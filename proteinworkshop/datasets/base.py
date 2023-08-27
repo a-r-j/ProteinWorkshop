c966ec46a41483cefddfad2b96f90fd72f93d1d3
Arian Jamasb <arian.jamasb@roche.com>
2023-08-27 20:17:21 +0200

    document dataset baseclass

--- proteinworkshop/datasets/base.py
+++ proteinworkshop/datasets/base.py
@@ -214,6 +214,54 @@ class ProteinDataset(Dataset):
         in_memory: bool = False,
         store_het: bool = False,
     ):
+        """Dataset for loading protein structures.
+
+        :param pdb_codes: List of PDB codes to load. This can also be a list
+            of identifiers to specific to your filenames if you have
+            pre-downloaded structures.
+        :type pdb_codes: List[str]
+        :param root: Path to root directory, defaults to ``None``.
+        :type root: Optional[str], optional
+        :param pdb_dir: Path to directory containing raw PDB files,
+            defaults to ``None``.
+        :type pdb_dir: Optional[str], optional
+        :param processed_dir: Directory to store processed data, defaults to
+            ``None``.
+        :type processed_dir: Optional[str], optional
+        :param pdb_paths: If specified, the dataset will load structures from
+            these paths instead of downloading them from the RCSB PDB or using
+            the identifies in ``pdb_codes``. This is useful if you have already
+            downloaded structures and want to use them. defaults to ``None``
+        :type pdb_paths: Optional[List[str]], optional
+        :param chains: List of chains to load for each PDB code,
+            defaults to ``None``.
+        :type chains: Optional[List[str]], optional
+        :param graph_labels: List of tensors to set as graph labels for each
+            examples. If not specified, no graph labels will be set.
+            defaults to ``None``.
+        :type graph_labels: Optional[List[torch.Tensor]], optional
+        :param node_labels: List of tensors to set as node labels for each
+            examples. If not specified, no node labels will be set.
+            defaults to ``None``.
+        :type node_labels: Optional[List[torch.Tensor]], optional
+        :param transform: List of transforms to apply to each example,
+            defaults to ``None``.
+        :type transform: Optional[List[Callable]], optional
+        :param pre_transform: _description_, defaults to None
+        :type pre_transform: Optional[Callable], optional
+        :param pre_filter: _description_, defaults to None
+        :type pre_filter: Optional[Callable], optional
+        :param log: _description_, defaults to True
+        :type log: bool, optional
+        :param overwrite: _description_, defaults to False
+        :type overwrite: bool, optional
+        :param format: _description_, defaults to "pdb"
+        :type format: Literal[&quot;mmtf&quot;, &quot;pdb&quot;], optional
+        :param in_memory: _description_, defaults to False
+        :type in_memory: bool, optional
+        :param store_het: _description_, defaults to False
+        :type store_het: bool, optional
+        """
         self.pdb_codes = [pdb.lower() for pdb in pdb_codes]
         self.pdb_dir = pdb_dir
         self.pdb_paths = pdb_paths
@@ -236,7 +284,19 @@ class ProteinDataset(Dataset):
             ]
 
     def download(self):
-        """Downloads PDB files not already present in the raw directory."""
+        """
+        Downloads structure files not already present in the raw directory
+        (``raw_dir``).
+
+        Structures are downloaded from the RCSB PDB using the Graphein
+        multiprocessed downloader.
+
+        Structure files are downloaded in ``self.format`` format (``mmtf`` or
+        ``pdb``). Downloading files in ``mmtf`` format is strongly recommended
+        as it will be both faster and smaller than ``pdb`` format.
+
+        Downloaded files are stored in ``self.raw_dir``.
+        """
         if self.pdb_codes is not None:
             to_download = (
                 self.pdb_codes
@@ -264,10 +324,20 @@ class ProteinDataset(Dataset):
 
     @property
     def raw_dir(self) -> str:
+        """Returns the path to the raw data directory.
+
+        :return: Raw data directory.
+        :rtype: str
+        """
         return os.path.join(self.root, "raw") if self.pdb_dir is None else self.pdb_dir  # type: ignore
 
     @property
     def raw_file_names(self) -> List[str]:
+        """Returns the raw file names.
+
+        :return: List of raw file names.
+        :rtype: List[str]
+        """
         if self.pdb_paths is None:
             return [f"{pdb}.{format}" for pdb in self.pdb_codes]
         else:
@@ -275,6 +345,14 @@ class ProteinDataset(Dataset):
 
     @property
     def processed_file_names(self) -> Union[str, List[str], Tuple]:
+        """Returns the processed file names.
+
+        This will either be a list in format [``{pdb_code}.pt``] or
+        a list of [{pdb_code}_{chain(s)}.pt].
+
+        :return: List of processed file names.
+        :rtype: Union[str, List[str], Tuple]
+        """
         if self.chains is not None:
             return [
                 f"{pdb}_{chain}.pt" for pdb, chain in zip(self.pdb_codes, self.chains)
@@ -283,6 +361,11 @@ class ProteinDataset(Dataset):
             return [f"{pdb}.pt" for pdb in self.pdb_codes]
 
     def process(self):
+        """Processes the raw data into PyTorch Geometric Data objects using
+        Graphein.
+
+        Processed data are stored in ``self.processed_dir`` as ``.pt`` files.
+        """
         if not self.overwrite:
             if self.chains is not None:
                 pdb_codes = [
@@ -346,10 +429,9 @@ class ProteinDataset(Dataset):
         else:
             fname = f"{self.pdb_codes[idx]}.pt"
 
-        return self.batch_format(torch.load(Path(self.processed_dir) / fname))
+        return self._batch_format(torch.load(Path(self.processed_dir) / fname))
 
-    def batch_format(self, x: Data) -> Data:
-        # x.coords = x.x
+    def _batch_format(self, x: Data) -> Data:
         # Set this to ensure proper batching behaviour
         x.x = torch.zeros(x.coords.shape[0])  # type: ignore
         x.amino_acid_one_hot = amino_acid_one_hot(x)
