a2f587f85961ebe1f30d1c51fba5c8dbf8859c7b
Arian Jamasb <arian.jamasb@roche.com>
2023-08-28 20:08:27 +0200

    clear docs

--- docs/build/_sources/modules/proteinworkshop.types.rst.txt
+++
@@ -1,5 +0,0 @@
-protein_workshop.types
-===============================
-
-.. automodule:: proteinworkshop.types
-    :members:
--- docs/render_emojis.sh
+++
@@ -1,7 +0,0 @@
-BUILDDIR=build  # Adapt to the location of your Sphinx output
-for i in "$BUILDDIR"/*.html; do
-    python emojize.py "$i" > "$i".new
-    # or emojize_pngorsvg.py "$i" > "$i".new
-    wdiff -3 "$i" "$i".new;
-    mv -vf "$i".new "$i"
-done
