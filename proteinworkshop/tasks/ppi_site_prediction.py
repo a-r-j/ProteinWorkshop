74004f81966a6c2f238d04370c86260ba1be8ff2
Jamasb <jamasba@sc1ns012is11.cm.cluster>
2023-11-22 16:16:36 -0800

    refactor chain identification

--- proteinworkshop/tasks/ppi_site_prediction.py
+++ proteinworkshop/tasks/ppi_site_prediction.py
@@ -30,18 +30,18 @@ class BindingSiteTransform(T.BaseTransform):
         self.fill_value = 1e-5
         self.ca_only = ca_only
         charstr: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.chain_map: Dict[str, int] = {charstr[i]: i for i in range(len(charstr))}
+        self.chain_map: Dict[str, int] = {
+            charstr[i]: i for i in range(len(charstr))
+        }
 
     def __call__(self, data: Union[Protein, Data]):
         # Map the chain labels to integers
         target_chains = []
 
+        chain_strs = [res.split(":")[0] for res in data.residue_id]
+
         for chain in data.graph_y:
-            try:
-                target_chains.append(self.chain_map[chain])
-            except KeyError:
-                # Sometimes chains are stored as integers
-                target_chains.append(int(chain) - 1)
+            target_chains.append(chain_strs.index(chain))
 
         target_chains = torch.tensor(target_chains)
         target_indices = torch.where(torch.isin(data.chains, target_chains))[0]
@@ -59,7 +59,9 @@ class BindingSiteTransform(T.BaseTransform):
         # Unwrap the coordinates
         other_chains = other_chains.reshape(-1, 3)
         # Remove any rows with 1e-5
-        other_chains = other_chains[~torch.all(other_chains == self.fill_value, dim=1)]
+        other_chains = other_chains[
+            ~torch.all(other_chains == self.fill_value, dim=1)
+        ]
 
         # Create a KDTree
         # If Ca only, we only see if the interacting chains are within the
