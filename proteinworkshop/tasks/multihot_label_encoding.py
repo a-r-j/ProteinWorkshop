735640b91f5c4f2dbbf2a588020769a38ee9938a
Jamasb <jamasba@sc1ns012is11.cm.cluster>
2023-12-26 13:50:56 -0800

    update tests

--- proteinworkshop/tasks/multihot_label_encoding.py
+++ proteinworkshop/tasks/multihot_label_encoding.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Set
 
 import torch
 from graphein.protein.tensor.data import Protein
@@ -16,6 +16,9 @@ class MultiHotLabelEncoding(T.BaseTransform):
 
     def __init__(self, num_classes: int) -> None:
         self.num_classes = num_classes
+
+    def required_batch_attributes(self) -> Set[str]:
+        return {"graph_y"}
 
     def __call__(self, data: Union[Protein, Data]) -> Union[Protein, Data]:
         labels = torch.zeros((1, self.num_classes))
