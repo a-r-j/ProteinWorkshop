dbd301f68209afaee683aa9a5acedb7190f3207a
kierandidi <kieran.didi@gmail.com>
2024-03-23 18:01:48 +0000

    added timesplit option and correct overwrite option

--- proteinworkshop/datasets/pdb_dataset.py
+++ proteinworkshop/datasets/pdb_dataset.py
@@ -2,6 +2,7 @@ from typing import Callable, Iterable, List, Optional, Dict, Literal
 
 import hydra
 import omegaconf
+import numpy as np
 import os
 import pandas as pd
 import pathlib
@@ -30,9 +31,10 @@ class PDBData:
         remove_non_standard_residues: bool,
         remove_pdb_unavailable: bool,
         train_val_test: List[float],
-        split_type: Literal["sequence_similarity", "random"],
+        split_type: Literal["sequence_similarity", "time_cutoff", "random"],
         split_sequence_similiarity: int,
-        overwrite_sequence_clusters: bool
+        overwrite_sequence_clusters: bool,
+        time_cutoffs_splits: List[str]
     ):
         self.fraction = fraction
         self.molecule_type = molecule_type
@@ -52,6 +54,7 @@ class PDBData:
         self.split_type = split_type
         self.split_sequence_similarity = split_sequence_similiarity
         self.overwrite_sequence_clusters = overwrite_sequence_clusters
+        self.time_cutoffs_splits = [np.datetime(date) for date in time_cutoffs_splits]
         self.splits = ["train", "val", "test"]
 
     def create_dataset(self):
@@ -131,6 +134,11 @@ class PDBData:
             pdb_manager.cluster(min_seq_id=self.split_sequence_similarity, update=True)
             splits = pdb_manager.split_clusters(
                 pdb_manager.df, update=True, overwrite = self.overwrite_sequence_clusters)
+        
+        elif self.split_type == "time_cutoff":
+            log.info(f"Splitting dataset via time_cutoff split into {self.train_val_test}...")
+            log.info(f"Using {self.time_cutoffs_splits} dates for split")
+            splits = pdb_manager.split_by_deposition_date(df=pdb_manager.df, update=True)
 
         log.info(splits["train"])
         return splits
